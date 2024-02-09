c044a5add3d0829629ab734eed94ab63cf92daa2
Pilzinsel64 <pilzinsel64@gmx.de>
2024-02-09 12:09:46 +0100

    spotlessApply

--- src/main/java/de/pilz/mystcraftextras/blocks/BlockBookstandAdv.java
+++ src/main/java/de/pilz/mystcraftextras/blocks/BlockBookstandAdv.java
@@ -1,8 +1,8 @@
 package de.pilz.mystcraftextras.blocks;
 
-import com.xcompwiz.mystcraft.block.BlockBookstand;
-
 import net.minecraft.block.material.Material;
+
+import com.xcompwiz.mystcraft.block.BlockBookstand;
 
 public class BlockBookstandAdv extends BlockBookstand {
 
---
+++ src/main/java/de/pilz/mystcraftextras/blocks/BlockLacernAdv.java
@@ -0,0 +1,12 @@
+package de.pilz.mystcraftextras.blocks;
+
+import net.minecraft.block.material.Material;
+
+import com.xcompwiz.mystcraft.block.BlockLectern;
+
+public class BlockLacernAdv extends BlockLectern {
+
+    public BlockLacernAdv(Material material) {
+        super(material);
+    }
+}
