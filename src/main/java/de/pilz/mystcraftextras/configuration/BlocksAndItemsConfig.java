a1787a238eaace39fb9d952dd3d41a853a1966a8
Pilzinsel64 <pilzinsel64@gmx.de>
2024-02-09 09:46:36 +0100

    add missing @Config for BlocksAndItemsConfig

--- src/main/java/de/pilz/mystcraftextras/configuration/BlocksAndItemsConfig.java
+++ src/main/java/de/pilz/mystcraftextras/configuration/BlocksAndItemsConfig.java
@@ -1,5 +1,10 @@
 package de.pilz.mystcraftextras.configuration;
 
+import com.gtnewhorizon.gtnhlib.config.Config;
+
+import de.pilz.mystcraftextras.MystcraftExtras;
+
+@Config(modid = MystcraftExtras.MODID, category = "blocksAndItems")
 public class BlocksAndItemsConfig {
 
 }
